7d9cab8b75bc8cc10b5ab0802d9bdb15a2b7baf5
mathias-vandencruijce <mathias.vandencruijce@student.ucll.be>
2022-03-02 15:09:40 +0100

    Added guide page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LandingPageComponent} from "./landing-page/landing-page.component";
 import {CreateMapComponent} from "./create-map/create-map.component";
+import {GuideComponent} from "./guide/guide.component";
 
 // you can't use routerLink="/" for your navigation because otherwise the active tab css doesn't work
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
-  {path: 'createMap', component: CreateMapComponent}
+  {path: 'createMap', component: CreateMapComponent},
+  {path: 'guide', component: GuideComponent}
 
 ];
 
